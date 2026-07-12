925193996ea9dacf8e100c5a957ef228d322505f
Michael Marmah <michael@rhema-systems.com>
2026-07-12 14:59:46 +0000

    Fix stock policy SQL backfill batch

--- docs/2026-07-12-stock-update-confirmation-policy.sql
+++ docs/2026-07-12-stock-update-confirmation-policy.sql
@@ -90,41 +90,51 @@ BEGIN TRY
       ADD [receiptStockConfirmedBy] NVARCHAR(1000) NULL;
   END;
 
-  UPDATE [dbo].[RetailOrg]
-    SET [stockUpdateMode] = N'AUTO'
-    WHERE [stockUpdateMode] IS NULL OR LTRIM(RTRIM([stockUpdateMode])) = N'';
+  /*
+    Keep these backfills dynamic so SQL Server compiles them after any ALTER TABLE
+    statements above have completed in the same run.
+  */
+  EXEC(N'
+    UPDATE [dbo].[RetailOrg]
+      SET [stockUpdateMode] = N''AUTO''
+      WHERE [stockUpdateMode] IS NULL OR LTRIM(RTRIM([stockUpdateMode])) = N'''';
+  ');
 
-  UPDATE [dbo].[GoodsReceipt]
-    SET
-      [stockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([stockUpdateStatus])), N''), N'POSTED'),
-      [stockConfirmedAt] = COALESCE([stockConfirmedAt], [postedAt]),
-      [stockConfirmedBy] = COALESCE([stockConfirmedBy], [operatorName], [sourceNodeCode], N'Legacy posted stock')
-    WHERE COALESCE(NULLIF(LTRIM(RTRIM([stockUpdateStatus])), N''), N'POSTED') = N'POSTED';
+  EXEC(N'
+    UPDATE [dbo].[GoodsReceipt]
+      SET
+        [stockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([stockUpdateStatus])), N''''), N''POSTED''),
+        [stockConfirmedAt] = COALESCE([stockConfirmedAt], [postedAt]),
+        [stockConfirmedBy] = COALESCE([stockConfirmedBy], [operatorName], [sourceNodeCode], N''Legacy posted stock'')
+      WHERE COALESCE(NULLIF(LTRIM(RTRIM([stockUpdateStatus])), N''''), N''POSTED'') = N''POSTED'';
+  ');
 
-  UPDATE [dbo].[InterStoreTransfer]
-    SET
-      [issueStockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([issueStockUpdateStatus])), N''), N'POSTED'),
-      [issueStockConfirmedAt] = CASE
-        WHEN [issueStockConfirmedAt] IS NULL AND [issuedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [issuedQuantity]) > 0
-          THEN [issuedAt]
-        ELSE [issueStockConfirmedAt]
-      END,
-      [issueStockConfirmedBy] = CASE
-        WHEN [issueStockConfirmedBy] IS NULL AND [issuedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [issuedQuantity]) > 0
-          THEN COALESCE([issueOperatorName], [sourceNodeCode], N'Legacy posted stock')
-        ELSE [issueStockConfirmedBy]
-      END,
-      [receiptStockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([receiptStockUpdateStatus])), N''), N'POSTED'),
-      [receiptStockConfirmedAt] = CASE
-        WHEN [receiptStockConfirmedAt] IS NULL AND [receivedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [receivedQuantity]) > 0
-          THEN [receivedAt]
-        ELSE [receiptStockConfirmedAt]
-      END,
-      [receiptStockConfirmedBy] = CASE
-        WHEN [receiptStockConfirmedBy] IS NULL AND [receivedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [receivedQuantity]) > 0
-          THEN COALESCE([receiptOperatorName], [destinationNodeCode], N'Legacy posted stock')
-        ELSE [receiptStockConfirmedBy]
-      END;
+  EXEC(N'
+    UPDATE [dbo].[InterStoreTransfer]
+      SET
+        [issueStockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([issueStockUpdateStatus])), N''''), N''POSTED''),
+        [issueStockConfirmedAt] = CASE
+          WHEN [issueStockConfirmedAt] IS NULL AND [issuedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [issuedQuantity]) > 0
+            THEN [issuedAt]
+          ELSE [issueStockConfirmedAt]
+        END,
+        [issueStockConfirmedBy] = CASE
+          WHEN [issueStockConfirmedBy] IS NULL AND [issuedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [issuedQuantity]) > 0
+            THEN COALESCE([issueOperatorName], [sourceNodeCode], N''Legacy posted stock'')
+          ELSE [issueStockConfirmedBy]
+        END,
+        [receiptStockUpdateStatus] = COALESCE(NULLIF(LTRIM(RTRIM([receiptStockUpdateStatus])), N''''), N''POSTED''),
+        [receiptStockConfirmedAt] = CASE
+          WHEN [receiptStockConfirmedAt] IS NULL AND [receivedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [receivedQuantity]) > 0
+            THEN [receivedAt]
+          ELSE [receiptStockConfirmedAt]
+        END,
+        [receiptStockConfirmedBy] = CASE
+          WHEN [receiptStockConfirmedBy] IS NULL AND [receivedAt] IS NOT NULL AND TRY_CONVERT(decimal(18, 3), [receivedQuantity]) > 0
+            THEN COALESCE([receiptOperatorName], [destinationNodeCode], N''Legacy posted stock'')
+          ELSE [receiptStockConfirmedBy]
+        END;
+  ');
 
   COMMIT TRANSACTION;
 END TRY
